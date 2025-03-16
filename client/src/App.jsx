e3dfd2314d2dc604cacd009dc98e60d30b3a61d0
Eshan Harshana <138109759+202320020818@users.noreply.github.com>
2025-03-16 19:08:47 +0530

    install react js and tailwind css and create the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+export default function main() {
+  return (
+    <div>
+      <h1 className="text-3xl text-red-500 absolute left-5 sm:left-100 top-5 w-28 sm:w-15 cursor-pointer">My React App</h1>
+      <p>Welcome to my React app!</p>
+    </div>
+  );
+}
+
